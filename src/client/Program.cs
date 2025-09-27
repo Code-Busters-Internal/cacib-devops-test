8c3859074d92782d83c79884775e677137483100
Aly-Bocar CISSE <alybocar.cisse@codebusters.fr>
2025-09-27 18:46:19 +0200

    feat: make client more dynamic.

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -4,8 +4,15 @@ using client;
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("http://localhost:8082");
 var client = new Greeter.GreeterClient(channel);
+string? me;
+do
+{
+    Console.WriteLine("Who are you ?");
+    me = Console.ReadLine();
+} while (string.IsNullOrWhiteSpace(me));
+
 var reply = await client.SayHelloAsync(
-    new HelloRequest { Name = "Whoever" });
-Console.WriteLine("Greeting: " + reply.Message);
+    new HelloRequest { Name = me });
+Console.WriteLine("From server: " + reply.Message);
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
