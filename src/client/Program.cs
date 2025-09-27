c2bd417d6da0f812e81c8d181896c4722c8b7df8
Aly-Bocar CISSE <alybocar.cisse@codebusters.fr>
2025-09-27 16:36:18 +0200

    chore: basic grpc client and server.

---
+++ src/client/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using client;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7164");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "Whoever" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
